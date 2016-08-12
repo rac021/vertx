7efefc31c09f98e03eb3165a03d029150d506785
Rachid Yahiaoui <ryahiaoui@ORL-5CB3410DK4.orleans.inra.fr>
2016-08-12 10:28:30 +0200

    vertx simple demo

---
+++ src/main/java/org/inra/vertx/HttpServer.java
@@ -0,0 +1,32 @@
+
+package org.inra.vertx;
+
+import io.vertx.core.Vertx;
+
+/**
+ *
+ * @author ryahiaoui
+ */
+public class HttpServer {
+   
+    public static void main(String[] args) {
+        
+        Vertx vertx = Vertx.vertx() ; 
+        
+        vertx.createHttpServer()
+             .requestHandler( request ->  {
+                 request.response().end("<h1> Hello from vertx :-) </h1> ");
+             })
+             .listen( 8080, ar -> {
+                 if(ar.failed()) {
+                     System.out.println(" Failed : " +ar.cause() );
+                 }
+                 else {
+                     System.out.println(" Server started ") ;
+                 }
+                 
+             })
+                
+        ;
+    }
+}
